1bcc01f414203dd8e1c2a142b89bb9187e8495a1
wade12 <wade@10.0.0.135>
2013-08-31 15:00:51 +0100

    fragments, fragments, fragments ...

--- CurrentWorkingProject/src/com/wade12/NewActivity.java
+++ CurrentWorkingProject/src/com/wade12/NewActivity.java
@@ -1,10 +1,9 @@
 package com.wade12;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.TextView;
 
 public class NewActivity extends BaseActivity {
@@ -16,40 +15,51 @@ public class NewActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.new_activity);
+		setContentView(R.layout.frame_activity);
 
-		Intent intent = getIntent();
-		String text = intent.getStringExtra("screenText");
-		Log.i(TAG, "Intent Text = " + text);
-		if (text != null) {
-			nameOut = (TextView) findViewById(R.id.name);
-			nameOut.setText(text);
-		} // end if
+		FragmentManager fragman = getSupportFragmentManager();
+		FragmentTransaction fragtrans = fragman.beginTransaction();
+		Fragment frag = new SideFragment();
+		fragtrans.replace(R.id.fragment_frame, frag);
+
+		/*
+		 * Intent intent = getIntent(); String text =
+		 * intent.getStringExtra("screenText"); Log.i(TAG, "Intent Text = " +
+		 * text); if (text != null) { nameOut = (TextView)
+		 * findViewById(R.id.name); nameOut.setText(text); } // end if
+		 */
 
 	} // end method onCreate
 
-	public void submitComment(View view) {
-
-		String name;
-		String comment;
-		String other;
-
-		EditText nameIn = (EditText) findViewById(R.id.nameIn);
-		EditText commentIn = (EditText) findViewById(R.id.commentIn);
-		EditText otherIn = (EditText) findViewById(R.id.otherIn);
-
-		name = nameIn.getText().toString();
-		comment = commentIn.getText().toString();
-		other = otherIn.getText().toString();
-
-		// TextView nameOut = (TextView) findViewById(R.id.name);
-		TextView commentOut = (TextView) findViewById(R.id.comment);
-		TextView otherOut = (TextView) findViewById(R.id.other);
-
-		nameOut.setText(name);
-		commentOut.setText(comment);
-		otherOut.setText(other);
-
-	} // end method submitComment
+	public void swapFragment() {
+		FragmentManager fragManager = getSupportFragmentManager();
+		FragmentTransaction fragTransaction = fragman.beginTransaction();
+		
+		Fragment current = 
+		
+		
+		
+	} // end method swapFragment
+	/*
+	 * public void submitComment(View view) {
+	 * 
+	 * String name; String comment; String other;
+	 * 
+	 * EditText nameIn = (EditText) findViewById(R.id.nameIn); EditText
+	 * commentIn = (EditText) findViewById(R.id.commentIn); EditText otherIn =
+	 * (EditText) findViewById(R.id.otherIn);
+	 * 
+	 * name = nameIn.getText().toString(); comment =
+	 * commentIn.getText().toString(); other = otherIn.getText().toString();
+	 * 
+	 * // TextView nameOut = (TextView) findViewById(R.id.name); TextView
+	 * commentOut = (TextView) findViewById(R.id.comment); TextView otherOut =
+	 * (TextView) findViewById(R.id.other);
+	 * 
+	 * nameOut.setText(name); commentOut.setText(comment);
+	 * otherOut.setText(other);
+	 * 
+	 * } // end method submitComment
+	 */
 
 } // end Class NewActivity
--- CurrentWorkingProject/src/com/wade12/SideFragment.java
+++ CurrentWorkingProject/src/com/wade12/SideFragment.java
@@ -5,14 +5,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SideFragment extends Fragment {
+
+	View view;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstatnceState) {
 
-		View view = inflater.inflate(R.layout.new_activity, container, false);
+		view = inflater.inflate(R.layout.new_activity, container, false);
+		Button button = (Button) view.findViewById(R.id.button1);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View view) {
+
+				String name;
+				String comment;
+				String other;
+
+				EditText nameIn = (EditText) view.findViewById(R.id.nameIn);
+				EditText commentIn = (EditText) view.findViewById(R.id.commentIn);
+				EditText otherIn = (EditText) view.findViewById(R.id.otherIn);
+
+				name = nameIn.getText().toString();
+				comment = commentIn.getText().toString();
+				other = otherIn.getText().toString();
+
+				TextView nameOut = (TextView) view.findViewById(R.id.name);
+				TextView commentOut = (TextView) view.findViewById(R.id.comment);
+				TextView otherOut = (TextView) view.findViewById(R.id.other);
+
+				nameOut.setText(name);
+				commentOut.setText(comment);
+				otherOut.setText(other);
+
+			} // end method onClick
+		}); // end OnclickListener
+
 		return view;
 	} // end method
-
 } // end Class SideFragment
